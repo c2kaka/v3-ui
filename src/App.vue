70f0a3feecb2f94271f828c2de585acee71b7b81
ouyangfan <ouyf@tiansu-china.com>
2021-04-27 21:28:50 +0800

    Home组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <div>
-    导航栏 |
-    <router-link to="/">Fan</router-link> |
-    <router-link to="/doc">Fan2</router-link>
-  </div>
-
-  <hr>
-
   <router-view />
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>Home</div>
+    <div class="banner">
+        <h1>V3-UI</h1>
+        <h2>based on Vue3 + TS</h2>
+        <p class="actions">
+            <a href="https://github.com/c2kaka/v3-ui">GitHub</a>
+            <router-link to="/doc">开始</router-link>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -8,6 +15,26 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    .banner {
+        padding: 100px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background: lightgreen;
+        > .actions{
+            padding: 8px 0;
+            a {
+                margin:0 8px;
+                background: #fff;
+                display: inline-block;
+                $h: 28px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
+        }
+    }
 </style>
